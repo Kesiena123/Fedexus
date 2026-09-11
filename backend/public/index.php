4ddac78c57f8b60e3aaf5828409b7bd0aa8a31c9
greatestmaxxx <greatestmaxxx@gmail.com>
2026-09-11 06:06:06 -0400

    Fix cPanel deploy layout and Laravel split-docroot bootstrap

--- backend/public/index.php
+++ backend/public/index.php
@@ -5,9 +5,15 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-require __DIR__.'/../vendor/autoload.php';
+$appPath = __DIR__.'/../';
+
+if (! is_dir($appPath.'vendor')) {
+    $appPath = __DIR__.'/../laravel-app/';
+}
+
+require $appPath.'vendor/autoload.php';
 
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once $appPath.'bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
